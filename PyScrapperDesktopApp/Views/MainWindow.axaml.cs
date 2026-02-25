bd72d9ae46a0d7d9e8f4887fa43c92949f9ce6ff
p50232 <Leon21brandstaetter@gmx.de>
2026-02-25 10:26:18 +0100

    1

---
+++ PyScrapperDesktopApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PyScrapperDesktopApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
